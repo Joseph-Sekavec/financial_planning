80c2b33b39bbe243e4cd85f4ddfb6c40c664782b
Joseph Sekavec <joseph.m.sekavec@gmail.com>
2024-09-20 11:48:15 -0500

    lets see

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-	echo "Hello world! financial planning.";
-?>
+<!DOCTYPE html>
+<html>
+
+<!-- <script src="fp.js"></script> -->
+
+<body>
+        <p>Welcome to totallynotascam corp. We here at totallynotascamc corp want to assist you with your budgeting needs. Please sign in, or click the signup button 
+		to give us your personal information. </p>
+		
+		<div>
+			<a href="login.html">Continue giving us your data! (login) </a>
+		</div>
+		
+		<div>
+			<a href="signup.html">Give us your data!</a>
+		</div>
+		
+</body>
+
+
+</html>
