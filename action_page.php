54bba30b181d3b30e9577d3d5726ea446377649b
Joseph Sekavec <joseph.m.sekavec@gmail.com>
2024-10-11 11:57:42 -0500

    using sqlite3

--- action_page.php
+++ action_page.php
@@ -1,7 +1,7 @@
-
+<html>
 <p>
 <?php
-
+/*
 	
 	// I need to connect to the database for users...
 	$servername = "localhost";
@@ -43,6 +43,45 @@
 		echo("GREAT \n");
 	}
 	?>
+	
+	*/
 	</p>
 	
-	</html>
+	
+
+
+	<p>
+		<?php
+
+			///////////////////// This is how to display the tables: /////////////////////
+			/*
+			$db = new SQLite3('budget.db');
+			$tablesquery = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
+			
+			
+			
+			while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
+				echo $table['name'] . '<br />';
+			}
+			*/
+			
+			
+			
+			/////////////////////// This is a test to make sure I can actually access the databases: //////////////////////////////////////////////////////////////
+					$connection = new SQLite3('budget.db');
+					if($connection){
+						echo "Connected\n";
+					}
+					$results = $connection->query('SELECT * FROM clients');
+					while($row=$results->fetchArray(SQLITE3_ASSOC)){
+						echo 'id = ' . $row['id'] . " ";
+						echo 'First Name = ' . $row['first_name'] . " ";
+						echo 'Last Name = ' . $row['last_name'] . '<br>';  // We separate <br> from the data by a '.'
+				
+					}
+			
+		?>
+	</p>
+
+	
+</html>
